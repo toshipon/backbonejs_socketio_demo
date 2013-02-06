753dd56a1f46ad38f8d9f73df3676547b5d5d61b
= <toshipon123@gmail.com>
2013-02-06 09:50:12 +0900

    add socket route

---
+++ routes/socket.coffee
@@ -0,0 +1,65 @@
+# Keep track of which names are used so that there are no duplicates
+userNames = (()->
+  names = {}
+
+  claim =  (name)->
+    if !name || names[name]
+      return false
+    else
+      names[name] = true
+      return true
+
+  # find the lowest unused "guest" name and claim it
+  getGuestName =  ()->
+    nextUserId = 1
+
+    loop
+      name = 'Guest' + nextUserId
+      nextUserId += 1
+      break unless !claim(name)
+
+    return name
+
+  # serialize claimed names as an array
+  get =  ()-> 
+    res = []
+    for user in names 
+      res.push(user)
+
+    return res
+
+  free =  (name)->
+    if names[name]
+      delete names[name]
+
+  return {
+    claim: claim,
+    free: free,
+    get: get,
+    getGuestName: getGuestName
+  }
+)()
+
+# export  for listening to the socket
+module.exports =  (socket)->
+  name = userNames.getGuestName()
+
+  # send the new user their name and a list of users
+  socket.emit 'init',
+    name: name,
+    users: userNames.get()
+
+  # notify other clients that a new user has joined
+  socket.broadcast.emit 'user:join',
+    name: name
+
+  # broadcast a user's message to other users
+  socket.on 'send:message',  (data)->
+    socket.broadcast.emit 'send:message',
+      user: name,
+      text: data.message
+
+  # clean up when a user leaves, and broadcast it to other users
+  socket.on 'disconnect',  ()->
+    socket.broadcast.emit 'user:left', name: name
+    userNames.free(name)
---
+++ routes/socket.js
@@ -0,0 +1,70 @@
+var userNames;
+
+userNames = (function() {
+  var claim, free, get, getGuestName, names;
+  names = {};
+  claim = function(name) {
+    if (!name || names[name]) {
+      return false;
+    } else {
+      names[name] = true;
+      return true;
+    }
+  };
+  getGuestName = function() {
+    var name, nextUserId;
+    nextUserId = 1;
+    while (true) {
+      name = 'Guest' + nextUserId;
+      nextUserId += 1;
+      if (!!claim(name)) {
+        break;
+      }
+    }
+    return name;
+  };
+  get = function() {
+    var res, user, _i, _len;
+    res = [];
+    for (_i = 0, _len = names.length; _i < _len; _i++) {
+      user = names[_i];
+      res.push(user);
+    }
+    return res;
+  };
+  free = function(name) {
+    if (names[name]) {
+      return delete names[name];
+    }
+  };
+  return {
+    claim: claim,
+    free: free,
+    get: get,
+    getGuestName: getGuestName
+  };
+})();
+
+module.exports = function(socket) {
+  var name;
+  name = userNames.getGuestName();
+  socket.emit('init', {
+    name: name,
+    users: userNames.get()
+  });
+  socket.broadcast.emit('user:join', {
+    name: name
+  });
+  socket.on('send:message', function(data) {
+    return socket.broadcast.emit('send:message', {
+      user: name,
+      text: data.message
+    });
+  });
+  return socket.on('disconnect', function() {
+    socket.broadcast.emit('user:left', {
+      name: name
+    });
+    return userNames.free(name);
+  });
+};
